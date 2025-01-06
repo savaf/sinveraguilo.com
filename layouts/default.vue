6caee2609bcd7e6295d0dc5f7bb7d7db00a081be
Your Name <you@example.com>
2025-01-06 09:45:02 -0400

    chore: remove SpeedInsights component from default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -40,7 +40,5 @@ useSeoMeta({
     <div class="flex space-x-2 mr-8 mb-6 right-0 bottom-0 z-50 fixed items-center sm:space-x-4">
       <LazyBackToTop />
     </div>
-
-    <SpeedInsights />
   </div>
 </template>
